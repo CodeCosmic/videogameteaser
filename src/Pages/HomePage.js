3e6a6c5f7eaf00b5539bade5dbc73fe299cf7531
Roger Martinez <mroger612@gmail.com>
2022-12-20 22:38:03 -0600

    The Hat man is here

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import { Container } from "reactstrap";
+
+
+const HomePage = () => {
+    return( 
+        <Container>
+            maybe SubHeader
+        </Container>
+    );
+};
+
+export default HomePage;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import HatMan from '../app/assets/img/hatMan.png'
+
+const Header = () => {
+    return (
+        <Navbar dark sticky='top'>
+            <NavbarBrand>
+                <img src={HatMan} alt='hatMan' />
+            </NavbarBrand>
+        </Navbar>
+    )
+}
+
+export default Header;
