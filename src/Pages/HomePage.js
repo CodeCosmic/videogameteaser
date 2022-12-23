10556fc5f7a10b3f8e451eb65968711fddac8a4c
Roger Martinez <mroger612@gmail.com>
2022-12-23 13:25:00 -0600

    Added Buttons

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import { Container } from "reactstrap";
+import ButtonChoices from "../components/ButtonChoices";
 
 
 const HomePage = () => {
     return( 
         <Container>
             maybe SubHeader
+            <ButtonChoices />
         </Container>
     );
 };
