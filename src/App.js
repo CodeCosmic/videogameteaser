2c307d0150dd82e42edfcdfa2b3cbdcbecad3929
Roger Martinez <mroger612@gmail.com>
2022-12-23 13:27:13 -0600

    wrong

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import ContactUs from './Pages/ContactUs';
 import Footer from './components/Footer';
-import HomePage from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path='/' element={<HomePage />} />
       </Routes>
       <Footer/>
     </div>
