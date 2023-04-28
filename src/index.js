28a2dc8329eda6f915bb4afb14a9f1ff2d479974
Roger Martinez <mroger612@gmail.com>
2023-04-27 20:27:59 -0500

    cleaned up the footer and removed store as it was not being used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.css';
 import App from './App';
@@ -11,11 +9,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
